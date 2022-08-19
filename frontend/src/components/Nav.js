0c1a6bd6e4a09e421c869244c3c1619001c8fa9c
Marien Florent <Florent62350@hotmail.fr>
2022-08-19 14:12:26 +0200

    add front - User profil nav

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,15 +1,27 @@
 import '../styles/Nav.css'
 
-function Nav({auth,setAuth,indexPage,setindexPage}) {
+function Nav({auth,setAuth,indexPage,setindexPage,profilData,setprofilData}) {
 	return (
 		<nav>
 			<ul>
+				<div>
 				<li><button onClick={()=>setindexPage(0)}>Profil</button></li>
-				<li><button onClick={()=>setindexPage(1)}>Liste d'actualité</button></li>
+				<li><button onClick={()=>setindexPage(1)}>Actualité</button></li>
 				<li><button onClick={()=>setindexPage(2)}>Recherche</button></li>
 				<li><button onClick={()=>setindexPage(3)}>Messages</button></li>
-				<li><button onClick={()=>setindexPage(4)}>Paramétre</button></li>
+				<li><button onClick={()=>setindexPage(4)}>Paramétres</button></li>
 				<li><button onClick={()=>setAuth([0])}>Déconnection</button></li>
+				</div>
+				{
+				profilData[0]!==0 ?
+				<li>
+					<button id="nav_button_UserInfo" onClick={()=>setindexPage(0)}>
+					<img src={ profilData.imageUrl } alt={profilData.name}/>
+					<p>{ profilData.name } { profilData.prename }</p>
+					</button>
+				</li>
+				:null
+				}
 			</ul>
 		</nav>
 	)
